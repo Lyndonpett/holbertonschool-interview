b5f3388f6c3a11e68bd6d179f0679b579c7f835e
lyndonpett <lyndonpett98@gmail.com>
2022-12-08 09:47:35 -0600

    Task 0

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Maria and Ben are playing a game. Given a set of consecutive integers starting from 1 up to and including n, they take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game.
-
-They play x rounds of the game, where n may be different for each round. Assuming Maria always goes first and both players play optimally, determine who the winner of each game is.
+"""
+Maria and Ben are playing a game. Given a set of consecutive integers
+starting from 1 up to and including n, they take turns choosing a prime
+number from the set and removing that number and its multiples from the set.
+The player that cannot make a move loses the game.
+They play x rounds of the game, where n may be different for each round.
+Assuming Maria always goes first and both players play optimally, determine who the winner of each game is.
 
     Prototype: def isWinner(x, nums)
     where x is the number of rounds and nums is an array of n
@@ -10,6 +14,7 @@ They play x rounds of the game, where n may be different for each round. Assumin
     You can assume n and x will not be larger than 10000
     You cannot import any packages in this task
 """
+
 
 def isWinner(x, nums):
     """Determines the winner of a game"""
